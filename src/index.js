bd4ba7f0e7507b677c9e195550021d5e63118d2f
DaryaKolbik <kolbikdarya@gmail.com>
2022-01-23 12:57:35 +0300

    feat: add solution to task reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+  let strN = String(Math.abs(n));
+  result = '';
+  for (i = strN.length - 1; i >= 0; i--) {
+	  result += strN[i]; 
+  }
+  return Number(result);
 }
